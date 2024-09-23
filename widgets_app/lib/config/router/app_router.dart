e7ed8d10239d2ba7cc150119717ad81b70f5e0d6
Gabriel Lorona <glorona@espol.edu.ec>
2024-09-22 21:18:17 -0500

    Named routes added

--- widgets_app/lib/config/router/app_router.dart
+++ widgets_app/lib/config/router/app_router.dart
@@ -5,18 +5,21 @@ import 'package:widgets_app/presentation/screens/screens.dart';
 final GoRouter appRouter = GoRouter(
   routes: [
     GoRoute(
+      name: HomeScreen.name,
       path: '/',
       builder: (context,state) {
         return const HomeScreen();
       },
     ),
     GoRoute(
+      name: ButtonsScreen.name,
       path: '/buttons',
       builder: (context,state) {
         return const ButtonsScreen();
       },
     ),
     GoRoute(
+      name: CardsScreen.name,
       path: '/cards',
       builder: (context,state) {
         return const CardsScreen();

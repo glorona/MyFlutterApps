59e6c06a0474daf55af52e824f494e3e016baef8
Gabriel Lorona <glorona@espol.edu.ec>
2025-02-25 18:43:56 -0500

    Shellroute impl

--- cinemapedia/lib/config/router/app_router.dart
+++ cinemapedia/lib/config/router/app_router.dart
@@ -1,25 +1,56 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-      //Child routes! Permite volver al home desde una ruta particular
+  
+  //Esta solucion no permite mantener el estado de las pantallas que se navegan, por lo que es lo max que se puede hacer.
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
-         GoRoute(
-          path: 'movie/:id', //Para enviar parametro
-          name: MovieScreen.name,
+        GoRoute(
+          path: '/',
           builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id', //Para enviar parametro
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
 
-            return MovieScreen(movieId: movieId);
-        }),
- 
-      ]
-      
-      
-      ),
- 
+                return MovieScreen(movieId: movieId);
+            }),
+          ]
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        )
+      ])
+
+  // //Rutas padre-hijo
+  // GoRoute(
+  //     path: '/',
+  //     name: HomeScreen.name,
+  //     builder: (context, state) => const HomeScreen(childView: HomeView(),),
+  //     //Child routes! Permite volver al home desde una ruta particular
+  //     routes: [
+  //        GoRoute(
+  //         path: 'movie/:id', //Para enviar parametro
+  //         name: MovieScreen.name,
+  //         builder: (context, state) {
+  //           final movieId = state.pathParameters['id'] ?? 'no-id';
+
+  //           return MovieScreen(movieId: movieId);
+  //       }),
+
+  //     ]
+
+  //     ),
 ]);
--- cinemapedia/lib/presentation/widgets/shared/custom_botton_navigationbar.dart
+++ cinemapedia/lib/presentation/widgets/shared/custom_botton_navigationbar.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottonNavigationbar extends StatelessWidget {
   const CustomBottonNavigationbar({super.key});
 
+  static int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).matchedLocation;
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(items: [
-      BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
-      BottomNavigationBarItem(icon: Icon(Icons.label_outlined), label: 'Categories'),
-      BottomNavigationBarItem(icon: Icon(Icons.favorite_outline_rounded), label: 'Favorites')
-    ]);
+    return BottomNavigationBar(
+        elevation: 0,
+        currentIndex: getCurrentIndex(context),
+        onTap: (index) => onItemTapped(context, index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outlined), label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline_rounded), label: 'Favorites')
+        ]);
   }
 }
